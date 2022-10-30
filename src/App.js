857f756a6b147ceec62c4810a7d80dda1c9d575a
Dhiaa Shalabi <dhiaa.shalabi@gmail.com>
2022-10-30 07:36:49 +0300

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import SearchMovies from "./searchMovies";
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+        <h1 className="title">React Movie Search</h1>
+        <SearchMovies/>
+      </div>
+  )
+}
+
+export default App
